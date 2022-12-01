ebc04b03c5aa402e5319d89e5cfa41a33c49e08f
codegoat2012 <119547951+codegoat2012@users.noreply.github.com>
2022-11-30 19:46:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function change() {
+    document.getElementById("h1").innerHTML = "sdlkfjdfhiuvuegugurehguehuhgu9eh"
+}
+function range(){
+    document.getElementById("h1").innerHTML= "SAAHIL HCDFXCGHHNNBJGXXDFDSFDFGHJMKJNFCXSADSDFGHJFHRRWESRDFGH"
+}
